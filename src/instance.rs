241c4a09726ace55dcff6237453181b527b39bae
Ada Null <ada@worldenddisk.com>
2026-02-21 16:50:26 +0000

    instance build out

--- src/instance.rs
+++ src/instance.rs
@@ -1,6 +1,11 @@
 use bladeink::story::Story;
-use godot::prelude::*;
+use godot::{classes::Json, prelude::*};
 
+#[derive(GodotClass)]
+#[class(base=Node, init)]
 pub struct InkStoryInstance {
+    #[export]
+    ink_json: Option<Gd<Json>>,
+
     ink_story: Option<Story>,
 }
